f419906aa6aec612c322240feda12d0c1574c7cd
choilettuce <tkdgnstkdcn@naver.com>
2022-11-09 07:52:03 +0900

    221109

---
+++ Circulator/CalC.java
@@ -0,0 +1,21 @@
+package Juhyeng;
+
+public class CalC {
+	public static void main(String args[]) {
+		
+	}
+
+	public int add(int a, int b) {
+		return a+b;
+	}
+	public int subtract(int a, int b) {
+		return a-b;
+	}
+	public int multiply(int a, int b) {
+		return a*b;
+	}
+	public int divide(int a, int b) {
+		return a/b;
+		
+	}
+}
